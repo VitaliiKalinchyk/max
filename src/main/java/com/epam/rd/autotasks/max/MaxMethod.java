17821daa8451894f19aab6bab426158fe254f16a
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-08 14:22:12 +0300

    MaxMethod

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,12 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.Arrays;
+
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int[] copyOfArray=values.clone();
+        Arrays.sort(copyOfArray);
+        return copyOfArray[copyOfArray.length-1];
+        //throw new UnsupportedOperationException();
     }
 }
